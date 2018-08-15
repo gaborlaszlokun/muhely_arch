7ea44dd89b8092477dbd8b87598ca7a18d99c723
Gábor László <laszlogabor91@gmail.com>
2018-08-15 10:39:23 +0200

    Code refactor

--- arch.php
+++ arch.php
@@ -9,9 +9,6 @@
 	<table id="menu">
 	<tr id="menu">
 	 <td id="search" onclick="window.location.href = 'search.php'"><strong>Keresés</strong></td>
-	 <!-- <td id="search"><strong><div id="button"><a href="search.php" style="text-decoration: none; color : white";>Keresés</a></div></strong></td>-->
-	 
-	 <!--<td id="browser"><strong>Böngészés</strong></td>-->
 	 </tr>
 	 </table>
 	<div id="browse" <?php if( isset($_GET['lapszam']) || isset($_POST['lapszam']) || isset($_GET['artid']))echo 'style="display:none;"' ?>>	
@@ -20,13 +17,18 @@
 		$year = 2003;
 		for($i = 0; $i < 4; $i++){
 			print "<tr>";
-			for($j = 0; $j < 4; $j++){	
-				print '<td id="'.$year.'"><strong>'.$year.'</strong></td>';
+			for($j = 0; $j < 4; $j++){
+				if(count(scandir("muhely/".$year."/")) == 2) {
+					print '<td style="color: #931328;background-color: white;"><strong>'.$year.'</strong></td>';
+				}
+				else{
+					print '<td id="'.$year.'" ><strong>'.$year.'</strong></td>';
+				}
 				$year++;
 			}
 			print "</tr>";
 		}	
-	 ?>	 
+	?>	 
 	</table>
 	</div>
 	<?php
@@ -52,12 +54,7 @@
 }
 					else {
 						print "<td onclick=\"window.location.href = 'muhely/".$year."/".$lapid."/index.html'\"><strong>".$year. " / ".$lap."</strong></td>";
-}
-					//print "<td onclick=\"window.location.href = 'muhely/".$year."/".$lapid."/index.html'\"><strong>".$year. " / ".$lap."</strong></td>";
-					
-					//print "<td onclick=\"window.location.href = 'arch.php?lapszam=".$lapid."'\"><strong>".$year. " / ".$lap."</strong></td>";
-					//print "<td onclick=\"window.location.href = 'arch.php?lapszam=".$lapid."'\"><strong>".$year. " / ".$lap."</strong></td>";
-					
+					}
 					$lap++;
 				}		
 				print "</tr>";
@@ -66,42 +63,6 @@
 			$np_year++;
 			print '</table></div>';
 		}
-	?> 
-
-	 <div id="fields" <?php if( isset($_POST['lapszam']) == false) echo 'style="display:none;"'?>>
-	 <!-- Ide kerül a dinamikusan felbukkanó kereső űrlap-->
-	 <form method="post" action="search.php" class="input-list style-2">
-		<br/><br/>
-		<input id="query" type="text" name="lapszam">
-		<select name="mit">
-			<option value="minden">Mindenben</option>
-			<option value="cim">Csak címben</option>
-			<option value="szerzo">Csak szerző</option>
-		</select>
-		<select name="rovat">
-			<option value="minden">Minden rovat</option>
-			<option value="korkep">BME Körkép</option>
-			<option value="kapun">Kapun kívül</option>
-			<option value="kozelet">Közélet</option>
-			<option value="sport">Sport</option>
-			<option value="tudtech">Tudomány és Technika</option>
-			<option value="magazin">Magazin</option>
-			<option value="nezopont">Nézőpont</option>
-			<option value="vita">Vitafórum</option>
-			<option value="gondolatok">Gondolatok</option>
-			<option value="uzleti">Üzleti Negyed</option>
-			<!-- És így tovább igény szerint ( főképp, ha korábban más rovatok is voltak! ) -->
-		</select>
-		  <input type="submit" value="Mehet!" class="input-list style-2">
-		  <input id="hide" type="button" value="Elrejt" class="input-list style-2">
-	 </form>
-	 </div>
-
-	</body>
-	<?php
-	//include 'connect.php';
-	//include 'browse.php';
-	//include 'search.php';
-	//include 'details.php';
 	?>
+	</body>
 </html>
--- search.php
+++ search.php
@@ -1,53 +1,50 @@
-﻿<script src="jquery-1.11.3.js"></script>
-<script type="text/javascript" src="script.js"></script> 
-<table id="menu">
-	<tr id="menu">
-	 <td id="browsr" onclick="window.location.href = 'arch.php'"><strong>Böngészés</strong></td>
-	 </tr>
-	 </table>
-<link rel='stylesheet' type='text/css' href='arch.css'/>
-<div id="fields">
-	 <form method="post" action="search.php" class="input-list style-2">
-		<br/><br/>
-		<input id="query" type="text" name="lapszam">
-		  <input type="submit" value="Mehet!" class="input-list style-2">
-	 </form>
-	 </div>
+﻿<html>
+	<head>
+		<title>Műhely cikkarchívum</title>
+		<script src="jquery-1.11.3.js"></script>
+		<script type="text/javascript" src="script.js"></script>
+		<link rel='stylesheet' type='text/css' href='arch.css'/>
+	</head>
+	<body>
+		<table id="menu">
+			<tr id="menu">
+				<td id="browsr" onclick="window.location.href = 'arch.php'"><strong>Böngészés</strong></td>
+			</tr>
+		</table>
 
-
-
-<?php
-	if( isset($_POST['lapszam']) ){
-		$statement=$_POST['lapszam'];
-		if (strlen($statement) < 3){
-			print "Hiba! Túl rövid kifejezést adtál meg!";
-		}
-		else{
-			print "Találatok a(z) <strong>".$statement."</strong> kifejezésre (";
-			/*print "<table style = 'width: 100%;'>";
-			for($i=0;$i<10;$i++){
-				print "<tr><td style='color: white;background-color: #931328;height:200px'><strong>Haha</strong></td></tr>";
-			}
-			print "</table>";
-			*/
-			try{
-				$statement = strtolower("%".$statement."%");
-				$myPDO = new PDO('sqlite:muhely.db');
-				$stmt = $myPDO->prepare("SELECT * FROM muhely WHERE LOWER(szoveg) LIKE :id;");
-				$stmt->execute(['id' => $statement]); 
-				$user = $stmt->fetchAll();
-				print sizeof($user)." db találat) :<br>";
-				foreach($user as $row)
-					{
-						print $row['id'] ." ". $row['ev']."<br>";
+		<div id="fields">
+			 <form method="post" action="search.php" class="input-list style-2">
+			<br/><br/>
+			<input id="query" type="text" name="lapszam">
+			<input type="submit" value="Mehet!" class="input-list style-2">
+			</form>
+		</div>
+		<?php
+			if( isset($_POST['lapszam']) ){
+				$statement=$_POST['lapszam'];
+				if (strlen($statement) < 3){
+					print "Hiba! Túl rövid kifejezést adtál meg!";
+				}
+				else{
+					print "Találatok a(z) <strong>".$statement."</strong> kifejezésre (";
+					try{
+						$statement = strtolower("%".$statement."%");
+						$myPDO = new PDO('sqlite:muhely.db');
+						$stmt = $myPDO->prepare("SELECT * FROM muhely WHERE LOWER(szoveg) LIKE :id;");
+						$stmt->execute(['id' => $statement]); 
+						$user = $stmt->fetchAll();
+						print sizeof($user)." db találat) :<br>";
+						foreach($user as $row)
+							{
+								print $row['id'] ." ". $row['ev']."<br>";
+								print "<img src='muhely/".$row['ev']."/".$row['id']."/thumbnails/1.jpg'><br>";
+							}
 					}
-			}
-			 catch (Exception $e) {
-				echo 'Caught exception: ',  $e->getMessage(), "\n";
-			}
-
-		}
-	}
-
-	
-?>
+					 catch (Exception $e) {
+						echo 'Caught exception: ',  $e->getMessage(), "\n";
+					}
+				}
+			}			
+		?>
+	</body>
+</html>
